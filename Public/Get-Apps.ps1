3da574539828a219fa199c26502cef3280198280
Chris DeLashmutt <cdelashmutt@pivotal.io>
2020-08-03 16:12:10 -0400

    Add option to call v2 API and pass spaces

--- Public/Get-Apps.ps1
+++ Public/Get-Apps.ps1
@@ -3,19 +3,27 @@
    Gets a collection CloudFoundry App objects
 .DESCRIPTION
    The Get-Apps cmdlet gets App objects from the CloudFoundry API
-.PARAMETER Space
-    Optional space to restrict apps retrieved from
-.PARAMETER Name
-    This parameter is the name of the space
+.PARAMETER Spaces
+   Limits the request to a list of (or single) space
 .EXAMPLE
    Get-CFToken -API "https://api.cf.my.domain.com" -Username "myusername" -Password "mypassword" | Set-Headers
-   Get-Apps | ConvertTo-Json
+   Get-Organizations | where {$_.Name -eq 'my-org'} | Get-Spaces | where {$_.Name -eq "my-space"} | Get-Apps | ConvertTo-Json
 #>
 function Get-Apps {
 
     [CmdletBinding()]
     [OutputType([psobject])]
-    param()
+    param(
+        [Parameter(ValueFromPipeline)]
+        [ValidateNotNullOrEmpty()]        
+        [psobject]
+        $Spaces,
+
+        [Parameter()]
+        [ValidateSet("v2","v3")]
+        [string]
+        $APIVersion="v3"
+    )
 
     begin {
         Write-Verbose "[$($MyInvocation.MyCommand.Name)] Function started"
@@ -23,7 +31,13 @@ function Get-Apps {
 
     process {
         Write-Debug "[$($MyInvocation.MyCommand.Name)] PSBoundParameters: $($PSBoundParameters | Out-String)"
-        Write-Output (Invoke-PagedGetRequest "/v3/apps")
+        $query_preamble="space_guids="
+        if("v2" -eq $APIVersion) {
+            $query_preamble="q=space_guid%20IN%20"
+        }
+        foreach($space in $Spaces) {
+            Write-Output (Invoke-PagedGetRequest "/$APIVersion/apps?$query_preamble$($space.guid)")
+        }
     }
 
     end {
